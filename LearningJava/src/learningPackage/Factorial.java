c647994e78eb24e32bc03b8b8faa952a0b2569da
Marcos <mvpersch@gmail.com>
2021-01-07 20:59:01 -0300

    Factorial class

---
+++ LearningJava/src/learningPackage/Factorial.java
@@ -0,0 +1,55 @@
+package learningPackage;
+
+import java.util.Scanner;
+
+public class Factorial{
+
+	public static void main(String[] args){
+		long n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");   
+		n=sc.nextLong();
+		sc.close();
+		Factorial.loopFactorial(n);
+		Factorial.recursiveFactorial(n);
+	}
+	
+	/*
+	 * Calculate Facotiral loop/recursive
+	 */
+	public static void calcFactorial() {
+        long n;
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter number: ");
+	    n=sc.nextLong();
+	    sc.close();
+	    Factorial.loopFactorial(n);
+	    Factorial.recursiveFactorial(n);    
+	}
+	
+	/*
+	 * Calculate Factorial using Loop
+	 */
+	public static void loopFactorial(long n){
+		int fact = 1;
+	    for(int i=1;i<=n;i++){
+	    	fact=fact*i;
+	    }
+	    System.out.println("Loop Factorial: "+fact);
+	}
+	
+	/*
+	 * Calculate Factorial using Recursion
+	 */
+	public static void recursiveFactorial(long n){
+		long fact = Factorial.calcRecursiveFactorial(n);
+		System.out.println("Recursive Factorial: "+fact);
+	}
+	
+	public static long calcRecursiveFactorial(long n){
+		if(n<=0)
+			return 1;
+	    return Factorial.calcRecursiveFactorial(n-1)*n; 
+	}
+
+}
--- LearningJava/src/learningPackage/Learning.java
+++ LearningJava/src/learningPackage/Learning.java
@@ -3,8 +3,7 @@ package learningPackage;
 public class Learning {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Factorial.calcFactorial();
 	}
 
 }
